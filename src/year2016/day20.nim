dc924cbb6d67f0b705ae637e01d4524139fa6755
Dan Seminara <semi225599@gmail.com>
2021-05-02 17:44:55 -0400

    2020 Day 20

---
+++ src/year2016/day20.nim
@@ -0,0 +1,34 @@
+import algorithm
+import math
+import sequtils
+import strutils
+import unpack
+
+type IpFilter = tuple
+  lo: int
+  hi: int
+
+iterator parseIpFilters(input: string): IpFilter =
+  var ips: seq[IpFilter]
+  for line in input.splitLines:
+    [a, b] <- line.split('-').map(parseInt)
+    ips.add((a, b))
+  ips.sort
+  var curr = ips[0]
+  for next in ips[1..^1]:
+    if next.lo <= curr.hi + 1:
+      curr.hi = max(curr.hi, next.hi)
+    else:
+      yield curr
+      curr = next
+  yield curr
+
+proc part1*(input: string): int =
+  for (a, b) in parseIpFilters(input):
+    if a > 0: return 0
+    else: return b+1
+
+proc part2*(input: string): int =
+  result = 2 ^ 32
+  for (a, b) in parseIpFilters(input):
+    result -= b - a + 1
