2ea8234f5ff11196003bc853245ab3bf317f110e
Dan Seminara <semi225599@gmail.com>
2021-05-02 10:37:35 -0400

    2016 Day 15

---
+++ src/year2016/day15.nim
@@ -0,0 +1,23 @@
+import fusion/matching
+import re
+import sequtils
+import strutils
+
+{.experimental: "caseStmtMacros".}
+
+import "../utils"
+
+proc parseDisc(line: string): (int, int) =
+  case findAll(line, re"\d+").map(parseInt):
+    of [@discNum, @modulo, _, @pos]:
+      (-pos - discNum, modulo)
+    else:
+      raiseAssert "Parse failure: " & line
+
+proc part1*(input: string): int =
+  input.splitLines.map(parseDisc).chineseRemainder
+
+const extra = "\nDisc #7 has 11 positions; at time=0, it is at position 0."
+
+proc part2*(input: string): int =
+  (input & extra).splitLines.map(parseDisc).chineseRemainder
