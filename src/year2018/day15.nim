73243fcefa24f40c006fdb36048bab96e76fd58e
Dan Seminara <semi225599@gmail.com>
2021-05-14 20:48:57 -0400

    Cleanup + count finished rounds

--- src/year2018/day15.nim
+++ src/year2018/day15.nim
@@ -9,13 +9,19 @@ import tables
 
 import "../utils"
 
+type Outcome = enum
+  Finished, ElfDied, EndedEarly
+
 proc parseGraph(input: string): seq[seq[(char, int)]] =
   for row in input.splitLines:
     result.add(@[])
     for v in row:
       result[^1].add((v, if v in "EG": 200 else: 0))
 
-proc neighbors(grid: var seq[seq[(char, int)]], coord: Coord): seq[Coord] =
+proc get[T](grid: var seq[seq[T]], pos: Coord): var T {.inline.} =
+  grid[pos[0]][pos[1]]
+
+proc neighbors(coord: Coord): seq[Coord] =
   # reading order
   [(-1, 0), (0, -1), (0, 1), (1, 0)].mapIt(it + coord)
 
@@ -25,50 +31,51 @@ proc findNextMove(grid: var seq[seq[(char, int)]], enemy: char, coord: Coord): O
   var frontier = [coord].toDeque
   while frontier.len > 0:
     var pos = frontier.popFirst
-    let neighbs = neighbors(grid, pos)
-    if neighbs.anyIt(grid[it[0]][it[1]][0] == enemy):
+    let neighbs = pos.neighbors
+    if neighbs.anyIt(grid.get(it)[0] == enemy):
       while pos in path:
         result = some(pos)
         pos = path[pos]
       break
     for n in neighbs:
-      if grid[n[0]][n[1]][0] == '.' and n notin visited:
+      if grid.get(n)[0] == '.' and n notin visited:
         visited.incl(n)
         path[n] = pos
         frontier.addLast(n)
 
-proc runRound(grid: var seq[seq[(char, int)]], elfPower: int, allowElfDeath: bool): bool =
+proc runRound(grid: var seq[seq[(char, int)]], elfPower: int, allowElfDeath: bool): Outcome =
+  var elves, goblins: int
   let units = collect(newSeq):
     for r, row in grid:
       for c, v in row:
         if v[0] in "EG":
+          if v[0] == 'E': inc elves else: inc goblins
           (r, c)
   for pos in units:
-    let v = grid[pos[0]][pos[1]]
+    if elves == 0 or goblins == 0:
+      return EndedEarly
+    let v = grid.get(pos)
     if v[0] notin "EG":
       continue
     let enemy = if v[0] == 'E': 'G' else: 'E'
     var pos = pos
     if Some(@p) ?= findNextMove(grid, enemy, pos):
-      grid[pos[0]][pos[1]] = ('.', 0)
-      grid[p[0]][p[1]] = v
+      grid.get(pos) = ('.', 0)
+      grid.get(p) = v
       pos = p
-    let targets = neighbors(grid, pos).filterIt(grid[it[0]][it[1]][0] == enemy)
+    let targets = pos.neighbors.filterIt(grid.get(it)[0] == enemy)
     if targets.len > 0:
       let pwr = if v[0] == 'E': elfPower else: 3
-      var (tPos, n) = ((0, 0), int.high)
-      for target in targets:
-        if grid[target[0]][target[1]][1] < n:
-          n = grid[target[0]][target[1]][1]
-          tPos = target
-      let (t, hp) = grid[tPos[0]][tPos[1]]
+      let tPos = targets.foldl(if grid.get(b) < grid.get(a): b else: a)
+      let (t, hp) = grid.get(tPos)
       if hp <= pwr:
         if not allowElfDeath and t == 'E':
-          return true
+          return ElfDied
         else:
-          grid[tPos[0]][tPos[1]] = ('.', 0)
+          if t == 'E': dec elves else: dec goblins
+          grid.get(tPos) = ('.', 0)
       else:
-        grid[tPos[0]][tPos[1]] = (t, hp - pwr)
+        grid.get(tPos) = (t, hp - pwr)
 
 proc score(grid: var seq[seq[(char, int)]], c: int): Option[int] =
   var elves, goblins: bool
@@ -86,20 +93,18 @@ proc score(grid: var seq[seq[(char, int)]], c: int): Option[int] =
 
 proc part1*(input: string): int =
   var grid = input.parseGraph
-  # Should be counting completed rounds, this fails example but works on real input
   for c in 0..int.high:
-    discard grid.runRound(3, true)
-    if Some(@sc) ?= grid.score(c):
+    let res = grid.runRound(3, true)
+    if Some(@sc) ?= grid.score(if res == Finished: c+1 else: c):
       return sc
 
 proc part2*(input: string): int =
   let gridStart = input.parseGraph
-  var elfPwr = 3
   for elfPwr in 3..int.high:
     var grid = gridStart
-    # Should be counting completed rounds, this fails example but works on real input
     for c in 0..int.high:
-      if grid.runRound(elfPwr, false):
+      let res = grid.runRound(elfPwr, false)
+      if res == ElfDied:
         break
-      if Some(@sc) ?= grid.score(c):
+      if Some(@sc) ?= grid.score(if res == Finished: c+1 else: c):
         return sc
