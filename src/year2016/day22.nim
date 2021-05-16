8d06b3093bc65f66fa0622649a258377a150b6a3
Dan Seminara <semi225599@gmail.com>
2021-05-16 00:52:23 -0400

    Speed up with inner proc rather than lambda capture

--- src/year2016/day22.nim
+++ src/year2016/day22.nim
@@ -3,7 +3,6 @@ import sequtils
 import sets
 import strscans
 import strutils
-import sugar
 import tables
 
 import "../utils"
@@ -27,14 +26,8 @@ proc part1*(input: string): int =
          nodes[j].used > 0 and nodes[j].used < nodes[i].avail:
         inc result
 
-proc neighbors(grid: Table[Coord, Node], st: (Coord, Coord)): iterator: (Coord, Coord) =
-  return iterator(): (Coord, Coord) =
-    let (o, t) = st
-    for d in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-      let o2 = o + d
-      if o2 in grid and grid[o2].used <= 100:
-        let next = (o2, if o2 == t: o else: t)
-        yield next
+proc heur(st: (Coord, Coord)): int =
+  (st[1] - st[0]).abs.sum + ((0, 0) - st[1]).abs.sum
 
 proc part2*(input: string): int =
   let nodes = input.splitLines[2..^1].map(parseNode)
@@ -46,6 +39,16 @@ proc part2*(input: string): int =
     if node.used == 0:
       opn = node.coord
     mx = max(mx, node.coord)
-  for (d, node) in bfs((opn, (mx[0], 0)), (st) => neighbors(grid, st)):
-    if node[1] == (0, 0):
+
+  proc neighbors(st: (Coord, Coord)): iterator: (Coord, Coord) =
+    return iterator(): (Coord, Coord) =
+      let (o, t) = st
+      for d in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        let o2 = o + d
+        if o2 in grid and grid[o2].used <= 100:
+          let next = (o2, if o2 == t: o else: t)
+          yield next
+
+  for (d, v) in bfs((opn, (mx[0], 0)), neighbors):
+    if v[1] == (0, 0):
       return d
