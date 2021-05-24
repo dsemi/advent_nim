c3aabe290660bf1f9e5c7e50aa3865e4a2374668
Dan Seminara <semi225599@gmail.com>
2021-05-23 21:59:17 -0400

    2019 Day 11

---
+++ src/year2019/day11.nim
@@ -0,0 +1,43 @@
+import fusion/matching
+import sequtils
+import tables
+
+import "../utils"
+import "intcode"
+
+{.experimental: "caseStmtMacros".}
+
+proc runRobot(prog: var Program, t: Table[Coord, int]): Table[Coord, int] =
+  var pos = (0, 0)
+  var dir = (0, -1)
+  result = t
+  while not prog.done:
+    prog.send([result.getOrDefault(pos, 0)])
+    prog.run
+    case toSeq(prog.recv):
+      of [@col, @d]:
+        result[pos] = col
+        dir *= (if d == 1: (0, 1) else: (0, -1))
+        pos += dir
+      else: raiseAssert "Invalid response"
+
+proc part1*(input: string): int =
+  var prog = input.parse
+  prog.runRobot(initTable[Coord, int]()).len
+
+proc draw(points: Table[Coord, int]): string =
+  var (minX, minY, maxX, maxY) = (int.high, int.high, int.low, int.low)
+  for (x, y) in points.keys:
+    minX = min(minX, x)
+    minY = min(minY, y)
+    maxX = max(maxX, x)
+    maxY = max(maxY, y)
+  result = "\n"
+  for y in minY..maxY:
+    for x in minX..maxX:
+      result &= (if points.getOrDefault((x, y), 0) == 0: ' ' else: '#')
+    result &= "\n"
+
+proc part2*(input: string): string =
+  var prog = input.parse
+  prog.runRobot({(0, 0): 1}.toTable).draw
