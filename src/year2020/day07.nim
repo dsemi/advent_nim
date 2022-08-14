b4c4fc98c995f87ae8cc53364a72314c3ba32f53
Dan Seminara <semi225599@gmail.com>
2022-08-13 20:34:10 -0400

    Improve perf

--- src/year2020/day07.nim
+++ src/year2020/day07.nim
@@ -1,28 +1,35 @@
 import fusion/matching
-import re
-import sequtils
+import strscans
 import strutils
-import sugar
 import tables
 
 proc parse(input: string): Table[string, seq[(int, string)]] =
   result = initTable[string, seq[(int, string)]]()
-  for line in input.splitlines:
-    let bgs = line.findAll(re"(\d+ )?\w+ \w+ bags?")
-    var vs = collect(newSeq):
-      for x in bgs[1 .. ^1]:
-        if "no other" notin x:
-          [@n, @bg] := x.rsplit(' ', 1)[0].split(' ', 1)
-          (n.parseInt, bg)
-    result[bgs[0].rsplit(' ', 1)[0]] = vs
-
-proc holdsShinyGold(m: Table[string, seq[(int, string)]], k: string): bool =
-  m[k].anyIt(it[1] == "shiny gold" or holdsShinyGold(m, it[1]))
+  for line in input.splitLines:
+    [@outerBag, @innerBags] := line.split(" bags contain ")
+    var bags = newSeq[(int, string)]()
+    for bag in innerBags.split(", "):
+      if bag == "no other bags.":
+        continue
+      var n: int
+      var name1, name2: string
+      doAssert bag.scanf("$i $w $w", n, name1, name2)
+      bags.add((n, name1 & " " & name2))
+    result[outerBag] = bags
 
 proc part1*(input: string): int =
   let m = parse(input)
-  for k in m.keys:
-    result += holdsShinyGold(m, k).int
+  var rev = initTable[string, seq[string]]()
+  for k, v in m.pairs:
+    for es in v:
+      rev.mgetOrPut(es[1], newSeq[string]()).add(k)
+  var stack = rev["shiny gold"]
+  var visited = initTable[string, bool]()
+  while stack.len > 0:
+    let v = stack.pop
+    if not visited.hasKeyOrPut(v, true):
+      inc result
+      stack.add(rev.getOrDefault(v, newSeq[string]()))
 
 proc countBags(m: Table[string, seq[(int, string)]], k: string): int =
   for (n, k2) in m[k]:
