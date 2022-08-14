d9df3bf93bd44a0c2efde01f9a82138b4624981d
Dan Seminara <semi225599@gmail.com>
2022-08-14 14:15:18 -0400

    perf

--- src/year2020/day24.nim
+++ src/year2020/day24.nim
@@ -2,7 +2,6 @@ import re
 import sequtils
 import sets
 import strutils
-import sugar
 import tables
 
 import "../utils"
@@ -17,7 +16,6 @@ let dirs = {
 }.toTable
 
 proc flipTiles(input: string): HashSet[Coord] =
-  result = initHashSet[Coord]()
   let m = toSeq(dirs.keys).join("|").re
   for line in input.splitlines:
     var coord = (0, 0)
@@ -28,15 +26,45 @@ proc flipTiles(input: string): HashSet[Coord] =
 proc part1*(input: string): int =
   flipTiles(input).len
 
+const steps = 100
+
 proc part2*(input: string): int =
   var tiles = flipTiles(input)
-  for _ in 1..100:
-    var adj = newCountTable[Coord]()
-    for t in tiles:
-      for d in dirs.values:
-        adj.inc(t + d)
-    tiles = collect(initHashSet):
-      for t, v in adj.pairs:
-        if (if t in tiles: v != 0 and v <= 2 else: v == 2):
-          {t}
-  tiles.len
+  var minX, minY = int.high
+  var maxX, maxY = int.low
+  for coord in tiles:
+    minX = min(minX, coord[0])
+    minY = min(minY, coord[1])
+    maxX = max(maxX, coord[0])
+    maxY = max(maxY, coord[1])
+  let xOffset = -minX + steps + 1
+  let yOffset = -minY + steps + 1
+  minX += xOffset
+  minY += yOffset
+  maxX += xOffset
+  maxY += yOffset
+  var grid = newSeqWith(maxY+steps+2, newSeq[bool](maxX+steps+2))
+  for tile in tiles:
+    grid[tile[1] + yOffset][tile[0] + xOffset] = true
+  var grid2: seq[seq[bool]]
+  deepcopy(grid2, grid)
+  for _ in 1..steps:
+    dec minX
+    dec minY
+    inc maxX
+    inc maxY
+    for r in minY..maxY:
+      for c in minX..maxX:
+        var adj = 0
+        for d in [(1, -1), (0, -1), (-1, 0), (-1, 1), (0, 1), (1, 0)]:
+          if grid[r+d[1]][c+d[0]]:
+            inc adj
+        if grid[r][c]:
+          grid2[r][c] = adj != 0 and adj <= 2
+        else:
+          grid2[r][c] = adj == 2
+    swap(grid, grid2)
+  for row in grid:
+    for v in row:
+      if v:
+        inc result
