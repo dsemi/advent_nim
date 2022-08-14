2d82f7bedde7090f297522872bc6b86aa8c66d00
Dan Seminara <semi225599@gmail.com>
2022-08-14 13:47:37 -0400

    perf

--- src/year2020/day01.nim
+++ src/year2020/day01.nim
@@ -1,17 +1,26 @@
+import algorithm
+import fusion/matching
+import options
 import sequtils
 import strutils
 
-proc part1*(input: string): int =
-  let ns = input.splitLines.map(parseInt)
-  for i in ns.low .. ns.high:
-    for j in i+1 .. ns.high:
-        if ns[i] + ns[j] == 2020:
-          return ns[i] * ns[j]
+{.experimental: "caseStmtMacros".}
 
-proc part2*(input: string): int =
-  let ns = input.splitLines.map(parseInt)
-  for i in ns.low .. ns.high:
-    for j in i+1 .. ns.high:
-      for k in j+1 .. ns.high:
-        if ns[i] + ns[j] + ns[k] == 2020:
-          return ns[i] * ns[j] * ns[k]
+proc solve(input: string, x: int): Option[int] =
+  let ns = input.splitLines.map(parseInt).sorted
+  proc go(n: int, c: int, xs: openArray[int]): Option[int] =
+    if n == 1:
+      if xs.binarySearch(c) >= 0:
+        return some(c)
+    else:
+      for x in xs:
+        case go(n-1, c-x, xs[xs.upperBound(x) .. ^1])
+        of Some(@x2):
+          return some(x * x2)
+  go(x, 2020, ns)
+
+proc part1*(input: string): Option[int] =
+  solve(input, 2)
+
+proc part2*(input: string): Option[int] =
+  solve(input, 3)
