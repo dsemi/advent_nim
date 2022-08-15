ceb0a57c9b83f42367c56f6646d1d15f29004f17
Dan Seminara <semi225599@gmail.com>
2022-08-15 11:09:24 -0400

    restructure

--- src/year2021/day04.nim
+++ src/year2021/day04.nim
@@ -1,44 +1,36 @@
 import fusion/matching
 import sequtils
 import strutils
-import sugar
 
-type Board = seq[seq[uint8]]
+type Board = object
+  grid: seq[seq[uint8]]
+  done: bool
 
 proc isWinner(board: Board, ns: set[uint8]): bool =
-  for i in 0..<board.len:
+  for i in 0..<board.grid.len:
     var all1, all2 = true
-    for j in 0..<board.len:
-      all1 = all1 and board[i][j] in ns
-      all2 = all2 and board[j][i] in ns
+    for j in 0..<board.grid.len:
+      all1 = all1 and board.grid[i][j] in ns
+      all2 = all2 and board.grid[j][i] in ns
     if all1 or all2:
       return true
 
 proc score(board: Board, ns: set[uint8]): int =
-  for row in board:
+  for row in board.grid:
     for v in row:
       if v notin ns:
         result += int(v)
 
 iterator winners(s: string): int =
   [@nums, all @bds] := s.split("\n\n")
-  var boards = collect(newSeq):
-    for board in bds:
-      collect(newSeq):
-        for row in board.splitlines:
-          collect(newSeq):
-            for v in row.splitWhitespace:
-              uint8(v.parseUInt)
+  var boards = bds.mapIt(Board(grid: it.splitLines.mapIt(it.splitWhitespace.mapIt(it.parseUint.uint8))))
   var ns: set[uint8]
   for n in nums.split(',').map(parseInt):
     ns.incl(uint8(n))
-    var winners: seq[int]
-    boards.keepIf(proc(board: Board): bool =
-                      result = not board.isWinner(ns)
-                      if not result:
-                        winners.add(board.score(ns)))
-    for w in winners:
-      yield w*n
+    for i, board in boards.mpairs:
+      if not board.done and board.isWinner(ns):
+        board.done = true
+        yield n * board.score(ns)
 
 proc part1*(input: string): int =
   for x in winners(input):
