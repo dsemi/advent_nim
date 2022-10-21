8497cb6b152e746a79d5e9d011b386ddb5e4485d
Dan Seminara <semi225599@gmail.com>
2022-10-21 11:18:53 -0400

    Parallel day 5, use array for day 14

--- src/year2016/day05.nim
+++ src/year2016/day05.nim
@@ -1,28 +1,53 @@
-import strutils
 import md5
+import strutils
+import threadpool
+{.experimental: "parallel".}
+
+const
+  largeBatchSize = 8
+  batchSize = 64000
+
+proc helper(seed: string, start: int): seq[uint8] =
+  for i in start .. start + batchSize:
+    let h = (seed & $i).toMD5
+    if h[0] == 0 and h[1] == 0 and h[2] < 16:
+      result.add(h[2])
 
 proc part1*(input: string): string =
   var
-    arr: array[8, string]
+    res: array[8, string]
     j = 0
-  for i in countup(0, int.high):
-    let h = (input & $i).toMD5
-    if h[0] == 0 and h[1] == 0 and h[2] < 16:
-      arr[j] = toHex(h[2], 1).toLowerAscii
-      inc j
-      if j == 8:
-        return arr.join
+  for sz in countup(0, int.high, largeBatchSize * batchSize):
+    var arr = newSeq[FlowVar[seq[uint8]]](largeBatchSize)
+    parallel:
+      for i in 0 .. arr.high:
+        arr[i] = spawn helper(input, sz + i * batchSize)
+    for grp in arr:
+      for x in ^grp:
+        res[j] = toHex(x, 1).toLowerAscii
+        inc j
+        if j == 8:
+          return res.join
+
+proc helper2(seed: string, start: int): seq[(uint8, uint8)] =
+  for i in start .. start + batchSize:
+    let h = (seed & $i).toMD5
+    if h[0] == 0 and h[1] == 0 and h[2] < 8:
+      result.add((h[2], h[3] shr 4))
 
 proc part2*(input: string): string =
   var
-    arr: array[8, string]
+    res: array[8, string]
     j = 0
-  for i in countup(0, int.high):
-    let h = (input & $i).toMD5
-    if h[0] == 0 and h[1] == 0 and h[2] < 8:
-      let pos = h[2]
-      if arr[pos] == "":
-        arr[pos] = toHex(h[3], 2)[0..0].toLowerAscii
-        inc j
-        if j == 8:
-          return arr.join
+  for sz in countup(0, int.high, largeBatchSize * batchSize):
+    var arr = newSeq[FlowVar[seq[(uint8, uint8)]]](largeBatchSize)
+    parallel:
+      for i in 0 .. arr.high:
+        arr[i] = spawn helper2(input, sz + i * batchSize)
+    for grp in arr:
+      for (n, c) in ^grp:
+        if res[n] == "":
+          res[n] = toHex(c, 1).toLowerAscii
+          inc j
+          if j == 8:
+            return res.join
--- src/year2016/day14.nim
+++ src/year2016/day14.nim
@@ -1,5 +1,4 @@
 import md5
-import tables
 import threadpool
 {.experimental: "parallel".}
 
@@ -10,8 +9,13 @@ proc hashNum(seed: string, n: int, num: int): string =
   for _ in 1..num:
     result = result.getMD5
 
+proc idx(c: char): int =
+  if c in '0'..'9':
+    return int(c) - int('0')
+  return int(c) - int('a') + 10
+
 iterator findIndexes(seed: string, num: int = 0): int =
-  var pot: Table[char, seq[int]]
+  var pot: array[16, seq[int]]
   for i1 in countup(0, int.high, batchSize):
     var arr = newSeq[FlowVar[string]](batchSize)
     parallel:
@@ -19,19 +23,16 @@ iterator findIndexes(seed: string, num: int = 0): int =
         arr[i] = spawn hashNum(seed, i+i1, num)
     for i in i1..<i1+batchSize:
       let hash = ^arr[i-i1]
-      var sent: set[char]
       for j in hash.low..hash.high-4:
         if hash[j] == hash[j+1] and hash[j] == hash[j+2] and
-           hash[j] == hash[j+3] and hash[j] == hash[j+4] and
-           hash[j] in pot and hash[j] notin sent:
-          sent.incl(hash[j])
-          for v in pot[hash[j]]:
+           hash[j] == hash[j+3] and hash[j] == hash[j+4]:
+          for v in pot[hash[j].idx]:
             if i - v <= 1000:
               yield v
-          pot[hash[j]] = @[]
+          pot[hash[j].idx].setLen(0)
       for j in hash.low..hash.high-2:
         if hash[j] == hash[j+1] and hash[j] == hash[j+2]:
-          pot.mgetOrPut(hash[j], @[]).add(i)
+          pot[hash[j].idx].add(i)
           break
 
 proc part1*(input: string): int =
