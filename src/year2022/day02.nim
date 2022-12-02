b9bc768d4e3c33be3079300564a8600c96bcfa4f
Dan Seminara <semi225599@gmail.com>
2022-12-02 01:18:33 -0500

    Shorten

--- src/year2022/day02.nim
+++ src/year2022/day02.nim
@@ -1,29 +1,18 @@
+import math
 import strutils
 import sugar
 
-proc solve(input: string, conv: (char, int) -> int): int =
+proc solve(input: string, conv: (int, int) -> int): int =
   for line in input.splitLines:
     let a = line[0].ord - 'A'.ord
-    let b = conv(line[2], a)
+    let b = conv(line[2].ord - 'X'.ord, a)
     let wld = if b == (a + 1) mod 3: 6
               elif b == a: 3
               else: 0
     result += wld + b + 1
 
 proc part1*(input: string): int =
-  proc conv(c: char, a: int): int =
-    case c:
-      of 'X': 0
-      of 'Y': 1
-      of 'Z': 2
-      else: raiseAssert "Malformed input"
-  solve(input, conv)
+  solve(input, (c, a) => c)
 
 proc part2*(input: string): int =
-  proc conv(c: char, a: int): int =
-    case c:
-      of 'X': (a + 2) mod 3
-      of 'Y': a
-      of 'Z': (a + 1) mod 3
-      else: raiseAssert "Malformed input"
-  solve(input, conv)
+  solve(input, (c, a) => floorMod(a + c - 1, 3))
