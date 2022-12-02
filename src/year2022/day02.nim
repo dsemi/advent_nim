6bf8f43e5be17dde1bea3b5208a93056b1638217
Dan Seminara <semi225599@gmail.com>
2022-12-02 11:51:54 -0500

    Shorten

--- src/year2022/day02.nim
+++ src/year2022/day02.nim
@@ -2,17 +2,12 @@ import math
 import strutils
 import sugar
 
-proc solve(input: string, conv: (int, int) -> int): int =
+proc solve(input: string, f: (int, int) -> int): int =
   for line in input.splitLines:
-    let a = line[0].ord - 'A'.ord
-    let b = conv(line[2].ord - 'X'.ord, a)
-    let wld = if b == (a + 1) mod 3: 6
-              elif b == a: 3
-              else: 0
-    result += wld + b + 1
+    result += f(line[0].ord - 'A'.ord, line[2].ord - 'X'.ord)
 
 proc part1*(input: string): int =
-  solve(input, (c, a) => c)
+  solve(input, (a, b) => 3*floorMod(b - a + 1, 3) + b + 1)
 
 proc part2*(input: string): int =
-  solve(input, (c, a) => floorMod(a + c - 1, 3))
+  solve(input, (a, b) => floorMod(a + b - 1, 3) + 3*b + 1)
