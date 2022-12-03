f552aebead4b2e703e51c76733781c96e8cc6405
Dan Seminara <semi225599@gmail.com>
2022-12-02 19:20:09 -0500

    Speed up 2017

--- src/year2017/day24.nim
+++ src/year2017/day24.nim
@@ -1,32 +1,45 @@
 import fusion/matching
-import options
 import sequtils
 import strutils
 import sugar
 
 type
-  Pipe = object
+  Pipe = ref object
     a: int
     b: int
     used: bool
 
-proc parsePipes(input: string): seq[Pipe] =
+  Builder[T] = ref object
+    a: seq[seq[Pipe]]
+    b: seq[seq[Pipe]]
+    f: proc(x: T, y: Pipe): T
+
+proc parsePipes(input: string): (seq[Pipe], int) =
   for line in input.splitLines:
     [@a, @b] := line.split('/').map(parseInt)
-    result.add(Pipe(a: a, b: b))
+    result[0].add(Pipe(a: a, b: b))
+    result[1] = max(result[1], max(a, b))
+
+proc build[T](b: Builder[T], port: int, curr: T): T =
+  result = curr
+  template run(x: untyped): untyped =
+    for pipe in x[port].mitems:
+      if not pipe.used:
+        pipe.used = true
+        result = max(result, b.build(pipe.a + pipe.b - port, b.f(curr, pipe)))
+        pipe.used = false
+  run(b.a)
+  run(b.b)
 
 proc solve[T](input: string, start: T, step: (T, Pipe) -> T): T =
-  var pipes = input.parsePipes
-  var start = start
-  proc dfs(pins: int, v: T) =
-    start = max(start, v)
-    for pipe in pipes.mitems:
-      if not pipe.used and (pipe.a == pins or pipe.b == pins):
-        pipe.used = true
-        dfs(if pipe.a == pins: pipe.b else: pipe.a, step(v, pipe))
-        pipe.used = false
-  dfs(0, start)
-  start
+  let (pipes, mx) = input.parsePipes
+  var a = newSeqWith(mx + 1, newSeq[Pipe]())
+  var b = newSeqWith(mx + 1, newSeq[Pipe]())
+  for pipe in pipes:
+    a[pipe.a].add(pipe)
+    if pipe.a != pipe.b:
+      b[pipe.b].add(pipe)
+  Builder[T](a: a, b: b, f: step).build(0, start)
 
 proc part1*(input: string): int =
   input.solve(0, (s, p) => s + p.a + p.b)
