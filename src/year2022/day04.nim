7b94fcf042d196dbfcc729abe7cb0e58b7a2d539
Dan Seminara <semi225599@gmail.com>
2022-12-04 02:20:36 -0500

    Use scanf instead of splitting

--- src/year2022/day04.nim
+++ src/year2022/day04.nim
@@ -1,13 +1,12 @@
-import fusion/matching
-import sequtils
+import strscans
 import strutils
 import sugar
 
 proc solve(input: string, f: (int, int, int, int) -> bool): int =
+  var a0, a1, b0, b1: int
   for line in input.splitLines:
-    [[@a0, @a1], [@b0, @b1]] := line.split(',').mapIt(it.split('-').map(parseInt))
-    if f(a0, a1, b0, b1):
-      inc result
+    doAssert line.scanf("$i-$i,$i-$i", a0, a1, b0, b1)
+    result += f(a0, a1, b0, b1).int
 
 proc part1*(input: string): int =
   input.solve((a0, a1, b0, b1) => a0 <= b0 and a1 >= b1 or b0 <= a0 and b1 >= a1)
