e917a75d20ad63a93cad253acf4e16d027eef657
Dan Seminara <semi225599@gmail.com>
2022-12-06 09:24:50 -0500

    Small improvements

--- src/year2022/day03.nim
+++ src/year2022/day03.nim
@@ -1,20 +1,19 @@
 import sequtils
+import std/setutils
 import strutils
 
 const s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 proc part1*(input: string): int =
   for line in input.splitLines:
-    var a, b: set[char]
-    for x in line[0 ..< line.len div 2]: a.incl(x)
-    for x in line[line.len div 2 .. ^1]: b.incl(x)
+    let a = toSet(line[0 ..< line.len div 2])
+    let b = toSet(line[line.len div 2 .. ^1])
     result += 1 + s.find(toSeq(a * b)[0])
 
 proc part2*(input: string): int =
   let comps = input.splitLines
   for i in countup(0, comps.high, 3):
-    var a, b, c: set[char]
-    for x in comps[i]: a.incl(x)
-    for x in comps[i+1]: b.incl(x)
-    for x in comps[i+2]: c.incl(x)
+    let a = toSet(comps[i])
+    let b = toSet(comps[i+1])
+    let c = toSet(comps[i+2])
     result += 1 + s.find(toSeq(a * b * c)[0])
--- src/year2022/day06.nim
+++ src/year2022/day06.nim
@@ -1,9 +1,9 @@
 import sequtils
 
 proc solve(input: string, nchars: int): int =
-  for i in 0..input.len-nchars:
-    if input[i ..< i+nchars].deduplicate.len == nchars:
-      return i + nchars
+  for i in nchars..input.len:
+    if input[i-nchars ..< i].deduplicate.len == nchars:
+      return i
 
 proc part1*(input: string): int =
   input.solve(4)
