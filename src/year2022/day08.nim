809f1dec3130657900c6b655527d36d6e40e7e22
Dan Seminara <semi225599@gmail.com>
2022-12-08 19:47:30 -0500

    Inline a bit more

--- src/year2022/day08.nim
+++ src/year2022/day08.nim
@@ -7,23 +7,22 @@ type Tree = object
 
 iterator trees(input: string): Tree =
   let grid = input.splitLines.mapIt(it.mapIt(($it).parseInt.uint8))
-  proc walk(val: uint8, iter: iterator: uint8): int =
-    for x in iter:
-      inc result
-      if x >= val:
-        break
   for r in grid.low .. grid.high:
     for c in grid[r].low .. grid[r].high:
       var tree = Tree(scenicScore: 1)
-      let toEdges = [
+      for path in [
         iterator(): uint8 = (for nr in countdown(r-1, grid.low): yield grid[nr][c]),
         iterator(): uint8 = (for nr in countup(r+1, grid.high): yield grid[nr][c]),
         iterator(): uint8 = (for nc in countdown(c-1, grid[r].low): yield grid[r][nc]),
         iterator(): uint8 = (for nc in countup(c+1, grid[r].high): yield grid[r][nc]),
-      ]
-      for path in toEdges:
-        tree.scenicScore *= walk(grid[r][c], path)
+      ]:
+        var cnt = 0
+        for x in path:
+          inc cnt
+          if x >= grid[r][c]:
+            break
         tree.visibleFromEdge = tree.visibleFromEdge or finished(path)
+        tree.scenicScore *= cnt
       yield tree
 
 proc part1*(input: string): int =
