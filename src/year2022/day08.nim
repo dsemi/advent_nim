3623918734be3eec7683688a3d09bb9514e9d693
Dan Seminara <semi225599@gmail.com>
2022-12-08 19:40:06 -0500

    Use closure iterators

--- src/year2022/day08.nim
+++ src/year2022/day08.nim
@@ -2,30 +2,28 @@ import sequtils
 import strutils
 
 type Tree = object
-  up: int
-  down: int
-  left: int
-  right: int
   visibleFromEdge: bool
+  scenicScore: int
 
 iterator trees(input: string): Tree =
   let grid = input.splitLines.mapIt(it.mapIt(($it).parseInt.uint8))
-  template check(x, iter, field, val) =
-    var edge = true
+  proc walk(val: uint8, iter: iterator: uint8): int =
     for x in iter:
-      inc tree.field
-      if val >= grid[r][c]:
-        edge = false
+      inc result
+      if x >= val:
         break
-    if edge:
-      tree.visibleFromEdge = true
   for r in grid.low .. grid.high:
     for c in grid[r].low .. grid[r].high:
-      var tree = Tree()
-      check(nr, countdown(r-1, grid.low), up, grid[nr][c])
-      check(nr, countup(r+1, grid.high), down, grid[nr][c])
-      check(nc, countdown(c-1, grid[r].low), left, grid[r][nc])
-      check(nc, countup(c+1, grid[r].high), right, grid[r][nc])
+      var tree = Tree(scenicScore: 1)
+      let toEdges = [
+        iterator(): uint8 = (for nr in countdown(r-1, grid.low): yield grid[nr][c]),
+        iterator(): uint8 = (for nr in countup(r+1, grid.high): yield grid[nr][c]),
+        iterator(): uint8 = (for nc in countdown(c-1, grid[r].low): yield grid[r][nc]),
+        iterator(): uint8 = (for nc in countup(c+1, grid[r].high): yield grid[r][nc]),
+      ]
+      for path in toEdges:
+        tree.scenicScore *= walk(grid[r][c], path)
+        tree.visibleFromEdge = tree.visibleFromEdge or finished(path)
       yield tree
 
 proc part1*(input: string): int =
@@ -34,4 +32,4 @@ proc part1*(input: string): int =
 
 proc part2*(input: string): int =
   for tree in input.trees:
-    result = max(result, tree.up * tree.down * tree.left * tree.right)
+    result = max(result, tree.scenicScore)
