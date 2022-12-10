189922462786f1fc56f5ff93420618080f8eff99
Dan Seminara <semi225599@gmail.com>
2022-12-10 11:57:09 -0500

    Small tidy

--- src/year2022/day10.nim
+++ src/year2022/day10.nim
@@ -1,22 +1,21 @@
 import std/enumerate
 import strutils
 
-iterator run(input: string): (int, int) =
+iterator run(input: string): int =
   var x = 1
-  for i, tok in enumerate(input.split):
-    yield (i+1, x)
+  for tok in input.split:
+    yield x
     case tok
     of "addx", "noop": discard
     else: x += tok.parseInt
 
 proc part1*(input: string): int =
-  for (cycle, x) in input.run:
-    if cycle in {20, 60, 100, 140, 180, 220}:
+  for cycle, x in enumerate(1, input.run):
+    if cycle mod 40 == 20:
       result += cycle * x
 
 proc part2*(input: string): string =
-  result = "\n"
-  for (cycle, x) in input.run:
-    result.add(if abs((cycle - 1) mod 40 - x) <= 1: '#' else: ' ')
-    if cycle < 240 and cycle mod 40 == 0:
-      result &= "\n"
+  for c, x in enumerate(input.run):
+    if c mod 40 == 0:
+      result &= '\n'
+    result.add(if abs(c mod 40 - x) <= 1: '#' else: ' ')
