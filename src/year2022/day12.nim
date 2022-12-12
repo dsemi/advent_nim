33acd3f437e66c51658779f04dd280fc5c6bbd15
Dan Seminara <semi225599@gmail.com>
2022-12-12 00:27:56 -0500

    2022 Day 12

---
+++ src/year2022/day12.nim
@@ -0,0 +1,46 @@
+import std/enumerate
+import strutils
+
+import "../utils"
+
+proc solve(input: string, p2: bool): int =
+  var starts = newSeq[Coord]()
+  var done: Coord
+  var grid: seq[seq[int]]
+  for r, line in enumerate(input.splitLines):
+    grid.add newSeq[int]()
+    for c, v in line:
+      case v
+      of 'S':
+        starts.add (r, c)
+        grid[^1].add(0)
+      of 'E':
+        done = (r, c)
+        grid[^1].add(25)
+      else:
+        if p2 and v == 'a':
+          starts.add (r, c)
+        grid[^1].add(v.ord - 'a'.ord)
+
+  proc neighbors(pos: Coord): iterator: Coord =
+    return iterator(): Coord =
+      let currH = grid[pos.x][pos.y]
+      for d in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        let pos2 = d + pos
+        if pos2.x in grid.low..grid.high and
+           pos2.y in grid[0].low..grid[0].high and
+           grid[pos2.x][pos2.y] <= currH + 1:
+          yield pos2
+
+  result = int.high
+  for start in starts:
+    for (d, p) in bfs(start, neighbors):
+      if p == done:
+        result = min(result, d)
+        break
+
+proc part1*(input: string): int =
+  input.solve(false)
+
+proc part2*(input: string): int =
+  input.solve(true)
