ee1d9ce3f354535af0a363ab96ee2d7588f6518c
Dan Seminara <semi225599@gmail.com>
2022-12-13 02:27:41 -0500

    Use builtin JSON parsing

--- src/year2022/day13.nim
+++ src/year2022/day13.nim
@@ -1,59 +1,33 @@
 import algorithm
+import json
 import sequtils
 import std/enumerate
-import streams
 import strutils
 
-type
-  pKind = enum
-    pLit,
-    pList
-  Packet = ref object
-    case kind: pKind
-    of pLit: val: int
-    of pList: vals: seq[Packet]
-
-proc parse(line: StringStream): Packet =
-  if line.peekChar == '[':
-    result = Packet(kind: pList)
-    discard line.readChar
-    while line.peekChar != ']':
-      result.vals.add(line.parse)
-      if line.peekChar == ',': discard line.readChar
-    discard line.readChar
-  else:
-    var n = 0
-    while line.peekChar in '0'..'9':
-      n = n * 10 + line.readChar.ord - '0'.ord
-    result = Packet(kind: pLit, val: n)
-
-proc cmp(a, b: Packet): int =
-  if a.kind == pLit and b.kind == pLit:
-    return cmp(a.val, b.val)
-  elif a.kind == pList and b.kind == pList:
-    for i in 0 .. min(a.vals.high, b.vals.high):
-      let c = cmp(a.vals[i], b.vals[i])
+proc cmp(a, b: JsonNode): int =
+  if a.kind == JInt and b.kind == JInt:
+    return cmp(a.num, b.num)
+  elif a.kind == JInt and b.kind == JArray:
+    return cmp(JsonNode(kind: JArray, elems: @[a]), b)
+  elif a.kind == JArray and b.kind == JInt:
+    return cmp(a, JsonNode(kind: JArray, elems: @[b]))
+  elif a.kind == JArray and b.kind == JArray:
+    for i in 0 .. min(a.elems.high, b.elems.high):
+      let c = cmp(a.elems[i], b.elems[i])
       if c != 0: return c
-    return cmp(a.vals.len, b.vals.len)
-  elif a.kind == pList and b.kind == pLit:
-    return cmp(a, Packet(kind: pList, vals: @[b]))
-  elif a.kind == pLit and b.kind == pList:
-    return cmp(Packet(kind: pList, vals: @[a]), b)
-
-proc `==`(a, b: Packet): bool =
-  cmp(a, b) == 0
+    return cmp(a.elems.len, b.elems.len)
 
 proc part1*(input: string): int =
   for i, packets in enumerate(1, input.split("\n\n")):
-    let pkts = packets.splitLines.mapIt(it.newStringStream.parse)
+    let pkts = packets.splitLines.mapIt(it.parseJson)
     if cmp(pkts[0], pkts[1]) < 0:
       result += i
 
 proc part2*(input: string): int =
-  let a = "[[2]]".newStringStream.parse
-  let b = "[[6]]".newStringStream.parse
+  let a = "[[2]]".parseJson
+  let b = "[[6]]".parseJson
   var pkts = @[a, b]
   for packets in input.split("\n\n"):
-    pkts.add packets.splitLines.mapIt(it.newStringStream.parse)
+    pkts.add packets.splitLines.mapIt(it.parseJson)
   pkts.sort(cmp = cmp)
   (pkts.find(a) + 1) * (pkts.find(b) + 1)
