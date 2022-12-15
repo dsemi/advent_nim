412c33c8c0b595eb58c668e8fcaaec69c4918cd2
Dan Seminara <semi225599@gmail.com>
2022-12-15 02:15:42 -0500

    Use intervals for part 1 to improve performance

--- src/year2022/day15.nim
+++ src/year2022/day15.nim
@@ -1,34 +1,54 @@
+import algorithm
+import intsets
 import sequtils
-import sets
 import strscans
 import strutils
 
 import "../utils"
 
+const p1Row = 2000000
+
 type Sensor = object
   pos: Coord
   dist: int
 
-proc parse(input: string): (seq[Sensor], HashSet[Coord]) =
+proc parse(input: string): (seq[Sensor], IntSet) =
   for line in input.splitLines:
     var sx, sy, bx, by: int
     doAssert line.scanf("Sensor at x=$i, y=$i: closest beacon is at x=$i, y=$i", sx, sy, bx, by)
     let dist = abs(sx - bx) + abs(sy - by)
     result[0].add Sensor(pos: (sx, sy), dist: dist)
-    result[1].incl((bx, by))
+    if by == p1Row: result[1].incl bx
+
+type Interval = object
+  lo, hi: int
+
+proc intersects(a, b: Interval): bool =
+  a.lo <= b.hi and b.lo <= a.hi
+
+proc union(a, b: Interval): Interval =
+  Interval(lo: min(a.lo, b.lo), hi: max(a.hi, b.hi))
+
+proc len(i: Interval): int =
+  i.hi - i.lo + 1
 
 proc part1*(input: string): int =
-  let (sensors, beacons) = input.parse
-  var minX = int.high
-  var maxX = int.low
-  for sensor in sensors:
-    minX = min(minX, sensor.pos.x - sensor.dist)
-    maxX = max(maxX, sensor.pos.x + sensor.dist)
+  let (sensors, bs) = input.parse
   let y = 2000000
-  for x in minX..maxX:
-    if (x, y) in beacons: continue
-    if sensors.anyIt(abs(it.pos.x - x) + abs(it.pos.y - y) <= it.dist):
-      inc result
+  var intervals = newSeq[Interval]()
+  for sensor in sensors:
+    let diff = sensor.dist - abs(sensor.pos.y - y)
+    if diff < 0: continue
+    intervals.add Interval(lo: sensor.pos.x - diff, hi: sensor.pos.x + diff)
+  intervals = intervals.sortedByIt(it.lo)
+  intervals = intervals.foldl(if intersects(a[^1], b): a[0..^2] & @[union(a[^1], b)]
+                              else: a & @[b], @[intervals[0]])
+  for interval in intervals:
+    result += interval.len
+  for x in bs:
+    for i in intervals:
+      if x in i.lo..i.hi:
+        dec result
 
 proc part2*(input: string): int =
   let sensors = input.parse[0]
