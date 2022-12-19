5784b6ec94394cedebd63111433eae4bb301ac81
Dan Seminara <semi225599@gmail.com>
2022-12-19 16:33:53 -0500

    Use simd to speed up

--- src/year2017/day15.nim
+++ src/year2017/day15.nim
@@ -1,37 +1,79 @@
+import bitops
 import fusion/matching
+import math
+import nimsimd/avx2
 import sequtils
 import strutils
-import sugar
 
-proc parse(input: string): (int, int) =
-  [@a, @b] := input.splitLines.mapIt(it.splitWhitespace[^1].parseInt)
+when defined(gcc) or defined(clang):
+  {.localPassc: "-mavx2".}
+
+const FactorA = 16807;
+const FactorB = 48271;
+let FactorA4 = mm256_set1_epi64x(984943658)
+let FactorB4 = mm256_set1_epi64x(1914720637)
+let Mod = mm256_set1_epi64x(0x7fffffff)
+let Lower16 = mm256_set1_epi64x(0xffff)
+let Zero = mm256_set1_epi64x(0)
+let Three = mm256_set1_epi64x(3)
+let Seven = mm256_set1_epi64x(7)
+
+proc parse(input: string): (uint64, uint64) =
+  [@a, @b] := input.splitLines.mapIt(it.splitWhitespace[^1].parseUInt.uint64)
   (a, b)
 
-proc judge(nA, nB: (int) -> int, n: int, gens: (int, int)): int =
-  var (a, b) = gens
-  for _ in 1..n:
-    result += int(uint16(a) == uint16(b))
-    a = nA(a)
-    b = nB(b)
+proc generate(x, factor: uint64): uint64 =
+  result = x * factor
+  result = (result shr 31) + (result and 0x7fffffff)
+  result = ((result shr 31) + result) and 0x7fffffff
 
-proc nextA(a: int): int =
-  a * 16807 mod 2147483647
+proc generate4(v, factor: M256i): M256i =
+  result = mm256_mul_epu32(v, factor)
+  let c = mm_set_epi32(0, 0, 0,31)
+  result = mm256_add_epi64(mm256_srl_epi64(result, c), mm256_and_si256(result, Mod))
+  result = mm256_and_si256(mm256_add_epi64(mm256_srl_epi64(result, c), result), Mod)
 
-proc nextB(b: int): int =
-  b * 48271 mod 2147483647
+proc part1*(input: string): uint64 =
+  let (a0, b0) = input.parse
+  var aNs = [a0, 0, 0, 0]
+  var bNs = [b0, 0, 0, 0]
+  for i in 1..3:
+    aNs[i] = generate(aNs[i-1], FactorA)
+    bNs[i] = generate(bNs[i-1], FactorB)
+  var a = mm256_setr_epi64x(aNs[0], aNs[1], aNs[2], aNs[3])
+  var b = mm256_setr_epi64x(bNs[0], bNs[1], bNs[2], bNs[3])
+  var res = Zero
+  for _ in 1..10_000_000:
+    res = mm256_sub_epi64(res, mm256_cmpeq_epi64(mm256_and_si256(a, Lower16),
+                                                 mm256_and_si256(b, Lower16)))
+    a = generate4(a, FactorA4)
+    b = generate4(b, FactorB4)
+  cast[array[4, uint64]](res).sum
 
-proc part1*(input: string): int =
-  judge(nextA, nextB, 40_000_000, parse(input))
-
-proc nextA2(a: int): int =
-  result = nextA(a)
-  while result mod 4 != 0:
-    result = nextA(result)
-
-proc nextB2(b: int): int =
-  result = nextB(b)
-  while result mod 8 != 0:
-    result = nextB(result)
+proc doMask(v, c: M256i): int32 =
+  mm256_movemask_pd(mm256_castsi256_pd(mm256_cmpeq_epi64(mm256_and_si256(v, c), Zero)))
 
 proc part2*(input: string): int =
-  judge(nextA2, nextB2, 5_000_000, parse(input))
+  let (a0, b0) = input.parse
+  var aNs = [a0, 0, 0, 0]
+  var bNs = [b0, 0, 0, 0]
+  for i in 1..3:
+    aNs[i] = generate(aNs[i-1], FactorA)
+    bNs[i] = generate(bNs[i-1], FactorB)
+  var a = mm256_setr_epi64x(aNs[0], aNs[1], aNs[2], aNs[3])
+  var b = mm256_setr_epi64x(bNs[0], bNs[1], bNs[2], bNs[3])
+  var maskA = doMask(a, Three)
+  var maskB = doMask(b, Seven)
+  for _ in 1..5_000_000:
+    while maskA == 0:
+      a = generate4(a, FactorA4)
+      maskA = doMask(a, Three)
+    while maskB == 0:
+      b = generate4(b, FactorB4)
+      maskB = doMask(b, Seven)
+    let idxA = maskA.countTrailingZeroBits
+    let idxB = maskB.countTrailingZeroBits
+    maskA = maskA xor (1'i32 shl idxA)
+    maskB = maskB xor (1'i32 shl idxB)
+    if (cast[array[4, uint64]](a)[idxA] and 0xffff) == (cast[array[4, uint64]](b)[idxB] and 0xffff):
+      inc result
