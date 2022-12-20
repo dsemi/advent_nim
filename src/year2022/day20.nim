aa2803b9cf8f0ce1802158783bb02163b5a525df
Dan Seminara <semi225599@gmail.com>
2022-12-20 03:10:56 -0500

    Small tidy

--- src/year2022/day20.nim
+++ src/year2022/day20.nim
@@ -4,7 +4,7 @@ import strutils
 import sugar
 
 proc mix(input: string, scale = 1, times = 1): int =
-  var ns = input.splitLines.mapIt(it.parseInt * scale)
+  let ns = input.splitLines.mapIt(it.parseInt * scale)
   var locs = collect(for i in ns.low..ns.high: i)
   for _ in 1..times:
     for i, n in ns:
@@ -12,8 +12,9 @@ proc mix(input: string, scale = 1, times = 1): int =
       locs.delete(loc)
       locs.insert(i, floorMod(n + loc, locs.len))
   let z = locs.find(ns.find(0))
-  for i in countup(z+1000, z+3000, 1000):
-    result += ns[locs[floorMod(i, locs.len)]]
+  result += ns[locs[floorMod(z + 1000, locs.len)]]
+  result += ns[locs[floorMod(z + 2000, locs.len)]]
+  result += ns[locs[floorMod(z + 3000, locs.len)]]
 
 proc part1*(input: string): int =
   input.mix
