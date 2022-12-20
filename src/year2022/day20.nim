8310a3f9eded26c88d3626e5fab1d87a0ff11ce0
Dan Seminara <semi225599@gmail.com>
2022-12-20 01:47:11 -0500

    Faster to just use seqs

--- src/year2022/day20.nim
+++ src/year2022/day20.nim
@@ -1,42 +1,25 @@
-import lists
 import math
 import sequtils
 import strutils
+import sugar
 
-proc nodes(input: string, scale = 1): seq[DoublyLinkedNode[int]] =
-  for i, n in input.splitLines.map(parseInt):
-    result.add newDoublyLinkedNode[int](n * scale)
-    if i > 0:
-      result[^2].next = result[^1]
-      result[^1].prev = result[^2]
-  result[0].prev = result[^1]
-  result[^1].next = result[0]
+proc nums(input: string, scale = 1): seq[int] =
+  input.splitLines.mapIt(it.parseInt * scale)
 
-proc mix(ns: seq[DoublyLinkedNode[int]], times = 1): int =
+proc mix(input: string, scale = 1, times = 1): int =
+  var ns = input.splitLines.mapIt(it.parseInt * scale)
+  var locs = collect(for i in ns.low..ns.high: i)
   for _ in 1..times:
-    for n in ns:
-      n.prev.next = n.next
-      n.next.prev = n.prev
-      var (a, b) = (n.prev, n.next)
-      let steps = floorMod(n.value, ns.len - 1)
-      for _ in 1..steps:
-        a = a.next
-        b = b.next
-      a.next = n
-      n.prev = a
-      b.prev = n
-      n.next = b
-  for n in ns:
-    if n.value == 0:
-      var n = n
-      for i in 1..3000:
-        n = n.next
-        if i mod 1000 == 0:
-          result += n.value
-      break
+    for i, n in ns:
+      let loc = locs.find(i)
+      locs.delete(loc)
+      locs.insert(i, floorMod(n + loc, locs.len))
+  let z = locs.find(ns.find(0))
+  for i in countup(z+1000, z+3000, 1000):
+    result += ns[locs[floorMod(i, locs.len)]]
 
 proc part1*(input: string): int =
-  input.nodes.mix
+  input.mix
 
 proc part2*(input: string): int =
-  input.nodes(811589153).mix(10)
+  input.mix(811589153, 10)
