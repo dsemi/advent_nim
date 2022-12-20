93dfa91fffcaaa8fa38e8943a1d55ee2e2ee99c0
Dan Seminara <semi225599@gmail.com>
2022-12-20 16:28:57 -0500

    Save m

--- src/year2022/day20.nim
+++ src/year2022/day20.nim
@@ -6,15 +6,16 @@ import sugar
 proc mix(input: string, scale = 1, times = 1): int =
   let ns = input.splitLines.mapIt(it.parseInt * scale)
   var locs = collect(for i in ns.low..ns.high: i)
+  let m = locs.len - 1
   let sz = sizeof(typeof(locs[0]))
   for _ in 1..times:
     for i, n in ns:
       let loc = locs.find(i)
-      if loc != locs.len - 1:
-        moveMem(addr locs[loc], addr locs[loc+1], sz * (locs.len - loc - 1))
-      let idx = floorMod(n + loc, locs.len - 1)
-      if idx != locs.len - 1:
-        moveMem(addr locs[idx+1], addr locs[idx], sz * (locs.len - idx - 1))
+      let idx = floorMod(n + loc, m)
+      if loc != m:
+        moveMem(addr locs[loc], addr locs[loc+1], sz * (m - loc))
+      if idx != m:
+        moveMem(addr locs[idx+1], addr locs[idx], sz * (m - idx))
       locs[idx] = i
   let z = locs.find(ns.find(0))
   result += ns[locs[floorMod(z + 1000, locs.len)]]
