7d420dc15f5913a0143961f2094003cd852e2d60
Dan Seminara <semi225599@gmail.com>
2022-12-22 02:01:33 -0500

    Small tidy

--- src/year2022/day22.nim
+++ src/year2022/day22.nim
@@ -21,25 +21,20 @@ proc walk(input: string, portals: Table[(Coord, Coord), (Coord, Coord)]): int =
       dir *= (0, -1)
     else:
       for _ in 1..instr.parseInt:
-        if (pos, dir) in portals:
-          let (pos2, dir2) = portals[(pos, dir)]
-          if grid[pos2] == '#':
-            break
-          pos = pos2
-          dir = dir2
-        else:
-          var pos2 = pos + dir
-          if grid[pos2] == '#':
-            break
-          pos = pos2
-  let finalRow = pos.x + 1
-  let finalCol = pos.y + 1
-  let finalFacing = if dir == (0, 1): 0
-                    elif dir == (1, 0): 1
-                    elif dir == (0, -1): 2
-                    elif dir == (-1, 0): 3
-                    else: raiseAssert "Invalid direction"
-  1000*finalRow + 4*finalCol + finalFacing
+        let (pos2, dir2) = if (pos, dir) in portals: portals[(pos, dir)]
+                           else: (pos + dir, dir)
+        if grid[pos2] == '#':
+          break
+        pos = pos2
+        dir = dir2
+  let row = pos.x + 1
+  let col = pos.y + 1
+  let facing = if dir == (0, 1): 0
+               elif dir == (1, 0): 1
+               elif dir == (0, -1): 2
+               elif dir == (-1, 0): 3
+               else: raiseAssert "Invalid direction"
+  1000*row + 4*col + facing
 
 proc part1*(input: string): int =
   var portals = initTable[(Coord, Coord), (Coord, Coord)]()
