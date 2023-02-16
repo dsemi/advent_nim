5899508700b5e2ab69fab3fa0084836d53213a2a
Dan Seminara <semi225599@gmail.com>
2023-02-16 16:46:05 -0500

    Simplify

--- src/year2022/day21.nim
+++ src/year2022/day21.nim
@@ -20,27 +20,24 @@ proc monkeys(input: string): TableRef[string, Monkey] =
       if pts.len == 2: Monkey(kind: Num, v: complex64(pts[1].parseInt.float64, 0))
       else: Monkey(kind: Math, l: pts[1], op: Op("+-*/".find(pts[2][0])), r: pts[3])
 
-proc eval(ms: TableRef[string, Monkey], k: string, p2: bool): Complex64 =
-  if p2 and k == "humn":
-    return im(1'f64)
+proc eval(ms: TableRef[string, Monkey], k: string): Complex64 =
   let m = ms[k]
   case m.kind
   of Num: m.v
   of Math:
     case m.op
-    of Add: eval(ms, m.l, p2) + eval(ms, m.r, p2)
-    of Sub: eval(ms, m.l, p2) - eval(ms, m.r, p2)
-    of Mul: eval(ms, m.l, p2) * eval(ms, m.r, p2)
-    of Div: eval(ms, m.l, p2) / eval(ms, m.r, p2)
+    of Add: eval(ms, m.l) + eval(ms, m.r)
+    of Sub: eval(ms, m.l) - eval(ms, m.r)
+    of Mul: eval(ms, m.l) * eval(ms, m.r)
+    of Div: eval(ms, m.l) / eval(ms, m.r)
 
 proc part1*(input: string): int =
-  input.monkeys.eval("root", false).re.int
+  input.monkeys.eval("root").re.int
 
 proc part2*(input: string): int =
-  let ms = input.monkeys
-  let root = ms["root"]
-  let l = ms.eval(root.l, true)
-  let r = ms.eval(root.r, true)
+  var ms = input.monkeys
   # We know the equation is linear
-  int(if l.im != 0: (r.re - l.re) / l.im
-      else: (l.re - r.re) / r.im)
+  ms["root"].op = Sub
+  ms["humn"] = Monkey(kind: Num, v: complex64(0, 1))
+  let n = ms.eval("root")
+  -int(n.re / n.im)
