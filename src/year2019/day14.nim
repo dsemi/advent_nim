0ad291dc073f73502219b5b7c11dc6ac25faf7da
Dan Seminara <semi225599@gmail.com>
2023-03-19 01:43:59 -0400

    Topological sort

--- src/year2019/day14.nim
+++ src/year2019/day14.nim
@@ -3,9 +3,12 @@ import strutils
 import sugar
 import tables
 
-type Reactions = Table[string, (int64, seq[(int64, string)])]
+type Reactions = ref object
+  graph: Table[string, (int64, seq[(int64, string)])]
+  topo: seq[string]
 
 proc parseReactions(input: string): Reactions =
+  var graph = initTable[string, (int64, seq[(int64, string)])]()
   for line in input.splitLines:
     [@ins, @outp] := line.split(" => ")
     let inps = collect(newSeq):
@@ -13,27 +16,32 @@ proc parseReactions(input: string): Reactions =
         [@n, @elem] := inp.split
         (n.parseBiggestInt, elem)
     [@n, @elem] := outp.split
-    result[elem] = (n.parseBiggestInt, inps)
+    graph[elem] = (n.parseBiggestInt, inps)
+  var incoming = initCountTable[string]()
+  for (_, srcs) in graph.values:
+    for (_, src) in srcs:
+      incoming.inc(src)
+  var topo = newSeq[string]()
+  var noIncoming = @["FUEL"]
+  while noIncoming.len > 0:
+    let e = noIncoming.pop
+    if e in graph:
+      topo.add(e)
+      for (_, m) in graph[e][1]:
+        incoming[m] = incoming[m] - 1
+        if incoming[m] == 0:
+          noIncoming.add(m)
+  Reactions(graph: graph, topo: topo)
 
 proc numOre(reactions: Reactions, n: int64): int64 =
-  var ore: int64
-  var surplus: Table[string, int64]
-  proc go(k: string, c: int64) =
-    if k in reactions:
-      let (n, chems) = reactions[k]
-      let (q, r) = (c div n, c mod n)
-      for (a, chem) in chems:
-        let amt = a * (if r != 0: q+1 else: q)
-        let val = surplus.getOrDefault(chem, 0)
-        surplus[chem] = max(0, val - amt)
-        if amt > val:
-          go(chem, amt - val)
-      if r != 0:
-        surplus[k] += n - r
-    else:
-      ore += c
-  go("FUEL", n)
-  ore
+  var cnts = initCountTable[string]()
+  cnts.inc("FUEL", n.int)
+  for e in reactions.topo:
+    let (n, srcs) = reactions.graph[e]
+    let k = (cnts[e] + n - 1) div n
+    for (n, m) in srcs:
+      cnts.inc(m, int(k * n))
+  cnts["ORE"]
 
 proc part1*(input: string): int64 =
   input.parseReactions.numOre(1)
