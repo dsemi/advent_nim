8f35ffd547ec2770a517b6a9c3b516b4cd56d26c
Dan Seminara <semi225599@gmail.com>
2024-01-13 02:29:35 -0500

    Use atomics and threading to avoid deprecated threadpool

--- src/year2015/day04.nim
+++ src/year2015/day04.nim
@@ -1,28 +1,39 @@
-import md5
-import threadpool
+import checksums/md5
+import std/atomics
+import std/cpuinfo
 
-proc part1*(input: string): int =
-  for i in 0 .. int.high:
-    let h = (input & $i).toMD5
-    if h[0] == 0 and h[1] == 0 and h[2] < 16:
-      return i
+const batchSize = 8000
 
-const
-  largeBatchSize = 8
-  batchSize = 8000
+type Shared = ref object
+  prefix: string
+  done: Atomic[bool]
+  counter: Atomic[uint32]
+  sol: Atomic[uint32]
 
-proc helper(seed: string, start: int): int =
-  for i in start ..< start + batchSize:
-    let h = (seed & $i).toMD5
-    if h[0] == 0 and h[1] == 0 and h[2] == 0:
-      return i
+proc worker(arg: (Shared, uint8)) {.thread.} =
+  let (s, v) = arg
+  while not s.done.load(moRelaxed):
+    let offset = s.counter.fetchAdd(batchSize, moRelaxed)
+    for n in 0'u32 ..< batchSize:
+      let i = offset + n
+      let h = (s.prefix & $i).toMD5
+      if h[0] == 0 and h[1] == 0 and h[2] <= v:
+        var val = s.sol.load(moRelaxed)
+        while i < val and not s.sol.compareExchangeWeak(val, i, moRelaxed):
+          discard
+        s.done.store(true, moRelaxed)
 
-proc part2*(input: string): int =
-  for sz in countup(0, int.high, largeBatchSize * batchSize):
-    var arr = newSeq[FlowVar[int]](largeBatchSize)
-    for i in 0 .. arr.high:
-      arr[i] = spawn helper(input, sz + i * batchSize)
-    for fx in arr:
-      let x = ^fx
-      if x > 0:
-        return x
+proc solve(input: string, v: uint8): uint32 =
+  let s = Shared(prefix: input)
+  s.sol.store(uint32.high)
+  var thr: array[20, Thread[(Shared, uint8)]]
+  for i in 0 ..< countProcessors():
+    createThread(thr[i], worker, (s, v))
+  joinThreads(thr)
+  s.sol.load
+
+proc part1*(input: string): uint32 =
+  input.solve(15)
+
+proc part2*(input: string): uint32 =
+  input.solve(0)
